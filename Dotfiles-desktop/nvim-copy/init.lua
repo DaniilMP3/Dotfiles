5d3b5f71e730a97a79b56a4382dcfd7ef91ff033
Dan Purisov <daniilpurisov390@gmail.com>
2026-01-30 12:32:07 +0200

    start assembling laptop dotfiles

---
+++ Dotfiles-desktop/nvim-copy/init.lua
@@ -0,0 +1,12 @@
+require("config.lazy")
+
+vim.opt.tabstop=4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.cindent = true
+vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.signcolumn = "yes"
+vim.opt.autoread = true
